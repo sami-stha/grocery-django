21b5413e2608172639512e9110a4d90f2e5897cb
Samikshya Shrestha <sami.college098@gmail.com>
2026-03-03 00:11:02 +0545

    render

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
-import dj_database_url 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'grocery',
+    'grocery',
 ]
 
 MIDDLEWARE = [
